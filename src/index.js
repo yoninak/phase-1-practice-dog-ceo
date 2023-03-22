37176c417f6d911b78f7ccc91c7ca3679504ad07
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-03-22 10:22:44 -0400

    w

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log('%c HI', 'color: firebrick')
+
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then((response) => response.json())
+.then((data) => data.message.forEach((imgUrl)=>{
+   let image = document.createElement("img")
+   image.src = imgUrl
+   let div = document.getElementById("dog-image-container")
+   div.append(image)
+}))
+
+
+
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then((response) => response.json())
+  .then((data) => {
+   var breeds = Object.keys(data.message)
+   console.log(typeof breeds)
+   breeds.foreach((listItems)=>{
+    let breedslist = document.createElement("li")
+    breedslist.textContent = listItems
+    let div = document.getElementById("dog-breeds")
+    div.append(li)
+   })
+
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
